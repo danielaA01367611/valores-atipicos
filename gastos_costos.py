17f52a430adeffcb620bb59111c4e32bf698f72e
danielaA01367611 <A01367611@tec.mx>
2024-04-29 23:09:28 -0600

    Segunda columna limpia

--- gastos_costos.py
+++ gastos_costos.py
@@ -13,7 +13,7 @@ df['FOLIO']=df['FOLIO'].fillna('DESCONOCIDO')
 
 df['GASTO']=df['GASTO'].fillna('GASTO NO RECONOCIDO')
 
-df['TC'] =df['TC'].fillna(method='bfill')
+df['TC'] =df['TC'].ffill()
 
 df['IMPORTE'] =df['IMPORTE'].fillna(round(df['IMPORTE'].mean(),1))
 
@@ -120,4 +120,67 @@ plt.ylabel('Frecuencia')
 fig = plt.figure(figsize =(5, 3))
 plt.boxplot(df["IVA"]) 
 plt.title("Outliers de IVA")
-plt.show()
+#plt.show()
+
+#MÉTODO ENCONTRANDO CUARTILES.
+y=df["IVA"]
+#print(y)
+
+percentile25=y.quantile(0.25) #Q1
+#print(percentile25)
+percentile75=y.quantile(0.75) #Q3
+#print(percentile75)
+iqr= percentile75 - percentile25
+#print(iqr)
+
+Limite_Superior_iqr= percentile75 + 1.5*iqr
+Limite_Inferior_iqr= percentile25 - 1.5*iqr
+#print("Limite superior permitido", Limite_Superior_iqr)
+#print("Limite inferior permitido", Limite_Inferior_iqr)
+
+#Obtenemos datos limpios
+data_clean_iqr= df[(y<=Limite_Superior_iqr)&(y>=Limite_Inferior_iqr)]
+#print(data_clean_iqr)
+
+#Realizamos diagrama de caja o bigotes limpio
+fig = plt.figure(figsize =(5, 3))
+plt.boxplot(data_clean_iqr["IVA"]) 
+plt.title("IVA sin outliers (metodo cuartiles)")
+#plt.show()
+
+#Realizamos histograma limpio
+fig = plt.figure(figsize =(7, 3))
+plt.hist(x=df["IVA"], color='blue', rwidth=0.50)
+plt.title('Histograma de IVA sin outliers (metodo cuartiles)')
+plt.xlabel('IVA')
+plt.ylabel('Frecuencia')
+#plt.show()
+
+#data_clean_iqr["IVA"].to_csv('IVA_sin_outliers.csv')
+
+#MÉTODO DESVIACIÓN ESTANDAR
+y=df["IVA"]
+Limite_Superior_dev_std= y.mean() + 3 * y.std()
+Limite_Inferior_dev_std= y.mean() - 3 * y.std()
+#print("Limite superior permitido usando desviación estandar", Limite_Superior_dev_std)
+#print("Limite inferior permitido usando desviación estandar", Limite_Inferior_dev_std)
+
+#Obtenemos datos limpios
+data_clean_dev_std= df[(y<=Limite_Superior_dev_std)&(y>=Limite_Inferior_dev_std)]
+#print(data_clean_dev_std)
+
+#Realizamos diagrama de caja o bigotes limpio
+fig = plt.figure(figsize =(5, 3))
+plt.boxplot(data_clean_iqr["IVA"]) 
+plt.title("IVA sin outliers (metodo std)")
+#plt.show()
+
+#Obtenemos histograma limpio
+fig = plt.figure(figsize =(7, 3))
+plt.hist(x=df["IVA"], color='blue', rwidth=0.50)
+plt.title('Histograma de IVA sin outliers con std')
+plt.xlabel('IVA')
+plt.ylabel('Frecuencia')
+#plt.show()
+
+data_clean_dev_std["IVA"].to_csv('IVA_sin_outliers(std).csv')
